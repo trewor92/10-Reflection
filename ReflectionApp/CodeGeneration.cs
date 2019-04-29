27fcfcae79cbc8e0fb8eb4b1754e28ae030787f4
Евгений <trewor.92@gmail.com>
2019-04-29 17:36:46 +0400

    task-10 initial commit

---
+++ ReflectionApp/CodeGeneration.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ReflectionApp
+{
+    public class CodeGeneration
+    {
+        /// <summary>
+        /// Returns the functions that returns vectors' scalar product:
+        /// (a1, a2,...,aN) * (b1, b2, ..., bN) = a1*b1 + a2*b2 + ... + aN*bN
+        /// Generally-обычно, CLR does not allow to implement such a method via generics to have one function for various number types:
+        /// int, long, float. double.
+        /// But it is possible to generate the method in the run time! 
+        /// See the idea of code generation using Expression Tree at: 
+        /// http://blogs.msdn.com/b/csharpfaq/archive/2009/09/14/generating-dynamic-methods-with-expression-trees-in-visual-studio-2010.aspx
+        /// </summary>
+        /// <typeparam name="T">number type (int, long, float etc)</typeparam>
+        /// <returns>
+        ///   The function that return scalar product of two vectors
+        ///   The generated dynamic method should be equal to static MultuplyVectors (see below).   
+        /// </returns>
+        public static Func<T[], T[], T> GetVectorMultiplyFunction<T>() where T : struct
+        {
+            var arParam1 = Expression.Parameter(typeof(T[]), "ar1");
+            var arParam2 = Expression.Parameter(typeof(T[]), "ar2");
+            var arIndex= Expression.Parameter(typeof(int), "i");
+           
+            
+            var result = Expression.Parameter(typeof(T), "result");
+            LabelTarget label = Expression.Label(typeof(T));
+
+
+            BlockExpression block = Expression.Block(new[] {result , arIndex },
+                Expression.Assign(result , Expression.Constant(default(T))), Expression.Assign(arIndex, Expression.Constant(0)),
+                Expression.Loop(
+                    Expression.IfThenElse(
+                        Expression.And(
+                            Expression.LessThan(arIndex, Expression.ArrayLength(arParam1)),
+                            Expression.LessThan(arIndex, Expression.ArrayLength(arParam2))),
+                        Expression.AddAssign(result,
+                            Expression.Multiply(
+                               Expression.ArrayIndex(arParam1, arIndex),
+                                Expression.ArrayIndex(arParam2, Expression.PostIncrementAssign(arIndex))
+                                )),
+                        Expression.Break(label, result)),
+                    label));
+
+
+
+            return Expression.Lambda<Func<T[], T[], T>>(block, arParam1, arParam2).Compile();
+
+
+            // TODO : Implement GetVectorMultiplyFunction<T>.
+            //throw new NotImplementedException();
+        }
+
+
+        // Static solution to check performance benchmarks
+        public static int MultuplyVectors(int[] first, int[] second)
+        {
+            int result = 0;
+            for (int i = 0; i < first.Length; i++)
+            {
+                result += first[i] * second[i];
+            }
+            return result;
+        }
+
+    }
+}
---
+++ ReflectionTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReflectionTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReflectionTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("77239a36-4f62-48b6-83e2-bc4a038de401")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
